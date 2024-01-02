fa1e4375b201bed88183e5d2715e7f2b2d0d91bd
Diego Canteros <123302060+turbina440@users.noreply.github.com>
2024-01-02 00:30:44 -0300

    Add files via upload

---
+++ map.js
@@ -0,0 +1,6 @@
+
+document.getElementById('mostrar-mapa').addEventListener('click',function(){
+document.getElementById('mapa').style.display='block';
+console.log("sigo probando");
+});
+/*no funciona con los div*/
